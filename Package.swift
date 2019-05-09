e6c9460f4687a51af2243799b72b28dbe6c1a789
Matthias Hollerbach <matzehollerbach@web.de>
2019-05-09 18:10:17 +0200

    add spm support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Swindler",
+    dependencies: [
+        .package(url: "https://github.com/kinglouie/AXSwift", .branch("master")),
+        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.8.0"),
+        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "7.3.1"))
+    ],
+    targets: [
+        .target(
+            name: "Swindler",
+            dependencies: ["AXSwift", "PromiseKit"]
+        )
+    ]
+)
